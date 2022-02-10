39b874a5015a63a4d86f700a394521ddbff9d522
Apoorv Varshney <85228469+apoorv-5369@users.noreply.github.com>
2022-02-10 23:44:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Solution {
         hm.put(1, 1); // Putting '1' as key with the same value.
         // Iterating from '2' to 'n'.
         for(int i=2; i<n+1;i++){
-            hm.put(i, hm.getOrDefault(i-1, 0)+hm.getOrDefault(i-2, 0)); //Putting values for ith fibonacci number by adding the previous two fibonacci numbers, which have already been present in the HashMap.
+            hm.put(i, hm.getOrDefault(i-1, 0)+hm.getOrDefault(i-2, 0)); //Putting values for ith fibonacci number by adding the previous two fibonacci numbers, which are already present in the HashMap.
         }
         return hm.get(n); // Finally, returning the value holded by the key 'n', which is the desired fibonacci number, F(n).
     }
